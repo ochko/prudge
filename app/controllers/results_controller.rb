682bd15dced133262f26f1298e363319e3a5ca32
Ochirkhuyag <ochkoo@gmail.com>
2010-05-26 01:20:43 +0900

    specify name strictly

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,7 +3,7 @@ class ResultsController < ApplicationController
   layout 'problems'
 
   def show
-    @result = ActiveRecord::Result.find(params[:id])
+    @result = ActiveRecord::Base::Result.find(params[:id])
 
     return if judge?
     
