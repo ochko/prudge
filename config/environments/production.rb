ecfa445ca0f03ca53bd25034ca11781accfed06e
Ochirkhuyag <ochkoo@gmail.com>
2010-06-05 20:31:26 +0900

    back to gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,11 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-ActionMailer::Base.delivery_method = :sendmail
-ActionMailer::Base.sendmail_settings = { 
-  :location       => '/usr/sbin/sendmail',
-  :arguments      => '-i -t -f no-reply@coder.mn'
+ActionMailer::Base.smtp_settings = { 
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true,
+  :user_name => "coder.mn@gmail.com",
+  :password => "taSmHPZ2FVguaxZ8wRWxGx8trsG3997"
 }
