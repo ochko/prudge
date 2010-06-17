e2bbf40b8ee0e31f0a28c230dddb606bba004d89
Ochirkhuyag <ochkoo@gmail.com>
2010-06-17 20:14:40 +0900

    problem list updated

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -1,9 +1,9 @@
 module ProblemsHelper
   def solved_or_not(correct)
     if correct == true
-      image_tag('ok.png')
+      image_tag('ok.png', :title => "Бодчихсон")
     elsif correct == false
-      image_tag('ng.png')
+      image_tag('ng.png', :title => "Бодож чадаагүй")
     else
       ''
     end
