b996140f0b2cb9a59cf57670b84a685eb82d8d47
Ochirkhuyag <ochkoo@gmail.com>
2010-08-23 10:24:58 +0900

    ranking shown properly

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,14 +10,13 @@ module UsersHelper
 
   def levels_legend(tag)
     legend = ''
-    Contest::LEVEL_NAMES.each do |level, name|
-      if level != 0
-        from = Contest::LEVEL_POINTS[level-1]
-        to = Contest::LEVEL_POINTS[level]
-        from = '' if from == 0
-        to = '' if to == 1000
-        legend << content_tag(tag, "#{name}: #{from}~#{to}", :class =>"rank_#{level}")
-      end
+    from = ''
+    [1, 2, 4, 8].each do |level|
+      name = Contest::LEVEL_NAMES[level]
+      to = Contest::LEVEL_POINTS[level]
+      to = '' if to == 1000
+      legend << content_tag(tag, "#{name}: #{from}~#{to}", :class =>"rank_#{level}")
+      from = Contest::LEVEL_POINTS[level]
     end 
     legend
   end
