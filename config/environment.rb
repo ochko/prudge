644ffc79d6d233583977109a218d15b3c328b5ef
ochko <ochkol@reallyenglish.com>
2013-03-20 23:46:13 +0900

    remove version

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,3 @@
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
