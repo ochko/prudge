998f702ab473a3efe75c26b288afb9195c56d25a
ochko <ochkol@reallyenglish.com>
2013-04-06 16:42:42 +0900

    update paperclip interpolations

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,13 +1,15 @@
-if defined?(Paperclip)
-  Paperclip.interpolates :user_id do |solution, style|
-    solution.instance.user_id
-  end
+Paperclip.interpolates :user_id do |solution, style|
+  solution.instance.user_id
+end
 
-  Paperclip.interpolates :problem_id do |solution, style|
-    solution.instance.problem_id
-  end
+Paperclip.interpolates :problem_id do |solution, style|
+  solution.instance.problem_id
+end
 
-  Paperclip.interpolates :repository do |solution, style|
-    Repo.path
-  end
+Paperclip.interpolates :repo_root do |solution, style|
+  Repo.root
+end
+
+Paperclip.interpolates :test_root do |solution, style|
+  Rails.root.join('judge','data')
 end
