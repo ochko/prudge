79c478da762da0171682e37f43831b99ece2f629
ochko <ochkol@reallyenglish.com>
2013-04-07 16:51:43 +0900

    refactor notification

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,37 +5,39 @@ class Ability
     alias_action :update, :create, :destroy, :to => :touch
     alias_action :update, :destroy, :to => :modify
 
-    if user
-      if user.admin?
-        can :manage, :all
-      elsif user.judge?
-        can :read, [Comment, Language, Page, Topic]
-        can :modify, Solution
-      else
-        can :read, Problem {|problem| user.owns?(problem) || problem.public? }
-        can :read, [Contest, Lesson, Topic, Comment, Language, Page]
-
-        can :check, Solution, :user_id => user.id, :state => 'updated'
-        can :read, Solution do |solution|
-          user.owns?(solution) ||
-            (!solution.competing? && user.solved?(solution.problem))
-        end
-        can :modify, Solution do |solution|
-          user.judge? || (user.owns?(solution) && solution.open?)
-        end
-        can :create, Solution do |solution|
-          contest = solution.contest
-          (contest.nil? || contest.finished? ||
-           (contest.started? && contest.openfor?(user))) &&
-            solution.fresh?
-        end
-      end
-      # all users
-      can :create, Lesson
-      can :modify, Lesson, :author_id => user.id
+    unless user
+      # not logged in
+      can :read, Page
+      can :read, Lesson
+      return
     end
-    # not logged in
-    can :read, Page
-    can :read, Lesson
+
+    if user.admin?
+      can :manage, :all
+    elsif user.judge?
+      can :read, [Comment, Language, Page, Topic]
+      can :modify, Solution
+    else
+      can :read, Problem {|problem| user.owns?(problem) || problem.public? }
+      can :read, [Contest, Lesson, Topic, Comment, Language, Page]
+
+      can :check, Solution, :user_id => user.id, :state => 'updated'
+      can :read, Solution do |solution|
+        user.owns?(solution) ||
+          (!solution.competing? && user.solved?(solution.problem))
+      end
+      can :modify, Solution do |solution|
+        user.judge? || (user.owns?(solution) && solution.open?)
+      end
+      can :create, Solution do |solution|
+        contest = solution.contest
+        (contest.nil? || contest.finished? ||
+         (contest.started? && contest.openfor?(user))) &&
+          solution.fresh?
+      end
+    end
+    # all users
+    can :create, Lesson
+    can :modify, Lesson, :author_id => user.id
   end
 end
--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,53 +3,48 @@ class Notifier < ActionMailer::Base
   default_url_options[:host] = "coder.query.mn"
   
   def password_reset_instructions(user)
-    subject       "Нууц үг сэргээх заавар"
-    from          "coder.mn@gmail.com"
-    recipients    user.email
-    sent_on       Time.now  
-    body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token),
-                  :login => user.login
+    user_notify(user, "Нууц үг сэргээх заавар")
   end
 
   def release_notification(user)
-    subject       "Кодер шинэчлэгдлээ"
-    from          "coder.mn@gmail.com"
-    recipients    user.email
-    sent_on       Time.now  
-    body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token),
-                  :login => user.login
+    user_notify(user, "Кодер шинэчлэгдлээ")
   end
 
   def problem_selection(user, contest, problem)
-    subject       "Таны дэвшүүлсэн бодлого тэмцээнд сонгогдлоо"
-    from          "coder.mn@gmail.com"
-    recipients    user.email
-    sent_on       Time.now  
-    body          :problem_url => problem_url(problem),
-                  :contest_url => contest_url(contest),
-                  :problem => problem, 
-                  :contest => contest,
-                  :login => user.login
+    compose(user, "Таны дэвшүүлсэн бодлого тэмцээнд сонгогдлоо",
+            :problem_url => problem_url(problem),
+            :contest_url => contest_url(contest),
+            :problem => problem, 
+            :contest => contest)
   end
 
   def new_contest(user, contest)
-    subject       "Кодер дээр Шинэ тэмцээн зарлагдлаа"
-    from          "coder.mn@gmail.com"
-    recipients    user.email
-    sent_on       Time.now  
-    body          :contest => contest,
-                  :contest_url => contest_url(contest),
-                  :login => user.login
+    contest_notify(user, contest, "Кодер дээр Шинэ тэмцээн зарлагдлаа")
   end
 
   def contest_update(user, contest)
-    subject       "Кодер дээр тэмцээнд өөрчлөлт орлоо"
+    contest_notify(user, contest, "Кодер дээр тэмцээнд өөрчлөлт орлоо")
+  end
+
+  private
+
+  def user_notify(user, title)
+    compose(user, title, 
+            :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
+  end
+
+  def contest_notify(user, contest, title)
+    compose(user, title, 
+            :contest => contest,
+            :contest_url => contest_url(contest))
+  end
+
+  def compose(user, title, options)
+    subject       title
     from          "coder.mn@gmail.com"
     recipients    user.email
     sent_on       Time.now  
-    body          :contest => contest,
-                  :contest_url => contest_url(contest),
-                  :login => user.login
+    body          options.merge(:login => user.login)
   end
 
 end
