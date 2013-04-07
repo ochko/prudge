fd53acb49d04448f07ddbb8648dabdd267dd1a24
ochko <ochkol@reallyenglish.com>
2013-04-07 17:00:26 +0900

    remove duplicated standing helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,19 +55,6 @@ module ApplicationHelper
     end
   end
 
-  def  standing(num)
-    if num > 3
-      return num
-    end
-    if num == 1
-      image_tag('cup-gold.png', :title=> 'Алт')
-    elsif num == 2
-      image_tag('cup-silver.png', :title=> 'Мөнгө')
-    elsif num == 3
-      image_tag('cup-bronze.png', :title=> 'Хүрэл')
-    end
-  end
-
   def medal_list(standings)
     list =''
     medal_color = 1
--- app/helpers/contests_helper.rb
+++ app/helpers/contests_helper.rb
@@ -1,18 +1,6 @@
+# -*- coding: utf-8 -*-
 module ContestsHelper
- def  medal_color(num)
-   if num > 3
-     return ''
-   end
-   if num == 1
-     image_tag('cup-gold.png', :title=> 'Алт')
-   elsif num == 2
-     image_tag('cup-silver.png', :title=> 'Мөнгө')
-   elsif num == 3
-     image_tag('cup-bronze.png', :title=> 'Хүрэл')
-   end
- end
-
- def show_place(num)
+ def standing(num)
    if num > 3
      return num
    end
