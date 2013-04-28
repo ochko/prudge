a2a54c241b6d00d01623e35f7be322ecafe4ea55
ochko <ochkol@reallyenglish.com>
2013-04-28 17:23:51 +0900

    remove unused table helpers

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -28,36 +28,4 @@ module ProblemsHelper
     end
   end
 
-  def results_table_horizontal(results)
-    matrix_to_table(humanized_results_matrix(results).transpose, "results-table")
-  end
-
-  def results_table_vertical(results)
-    matrix_to_table(humanized_results_matrix(results), "results-table")
-  end
-
-  private
-
-  def humanized_results_matrix(results)
-    header = ['Тэст', '', 'Ажиллагаа', 'Хугацаа', 'Санах ой', 'Хариу']
-    index = 0
-    results.reduce([header]) do |matrix, result|
-      matrix << [index += 1,
-                 render_viewable(result.hidden), 
-                 translate_message(result.execution),
-                 sec2milisec(result.time), 
-                 result.memory, 
-                 link_to(show_correctness(result.correct?), result)]
-    end
-  end
-
-  def matrix_to_table(matrix, id)
-    content = ["<table id='#{id}' class='table table-bordered'>"]
-    matrix.each do |row|
-      content << "<tr><td>" << row.join('</td><td>') << '</td></tr>'
-    end
-    content << "</table>"
-    content.join
-  end
-
 end
