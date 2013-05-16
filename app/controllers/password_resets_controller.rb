d2ae4da56b3ff694bb8e258bc7549c78fb60b518
ochko <ochkol@reallyenglish.com>
2013-05-17 00:45:55 +0900

    use flash_notice in password reset messages

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,44 +2,44 @@
 class PasswordResetsController < ApplicationController
   before_filter :load_user_using_perishable_token, :only => [:edit, :update]
   before_filter :require_no_user
-  
-  def new  
-  end  
-  
+
+  def new
+  end
+
   def create
     @user = User.find_by_email(params[:email])
     if @user
       @user.send_password_reset_instructions!
-      flash[:notice] = message_for :password_reset_instructions
+      flash_notice :password_reset_instructions
       redirect_to root_url
     else
-      flash[:notice] = message_for :user_not_found_by_email
+      flash_notice :user_not_found_by_email
       render :action => :new
     end  
   end
-  
+
   def edit
   end
- 
+
   def update
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.save
-      flash[:notice] = message_for :password_updated
+      flash_notice :password_updated
       redirect_to account_users_url
     else
       render :action => :edit
     end
   end
-  
-  private  
-  
-  def load_user_using_perishable_token  
-    @user = User.find_using_perishable_token(params[:id])  
-    unless @user  
-      flash[:notice] = message_for :password_reset_not_found
-      redirect_to root_url  
-    end  
+
+  private
+
+  def load_user_using_perishable_token
+    @user = User.find_using_perishable_token(params[:id])
+    unless @user
+      flash_notice :password_reset_not_found
+      redirect_to root_url
+    end
   end
-  
+
 end  
