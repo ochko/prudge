a777a170a3cb3e017c6bfb478d384a8494d87c34
ochko <ochkol@reallyenglish.com>
2013-05-26 23:10:36 +0900

    show latest post first

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
+    @posts = @user.posts.order('created_at desc')
     render :partial => 'index'
   end
 
