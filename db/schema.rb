2fa5cf9199ce1249ab66a1cec144a6b9fe07e727
ochko <ochkol@reallyenglish.com>
2013-06-03 22:35:12 +0900

    drop delayed_jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130503131614) do
+ActiveRecord::Schema.define(:version => 20130603133330) do
 
   create_table "comments", :force => true do |t|
     t.integer  "topic_id",                 :null => false
@@ -43,21 +43,6 @@ ActiveRecord::Schema.define(:version => 20130503131614) do
   end
 
   add_index "contests_users", ["contest_id", "user_id"], :name => "index_contests_users_on_contest_id_and_user_id", :unique => true
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "lessons", :force => true do |t|
     t.integer  "author_id",                        :null => false
