0bd99e80b680c6b2fd007295b2d9253b21df4d9d
Zolzaya <zolzaya.erdenebaatar@gmail.com>
2013-08-09 00:51:56 +0800

    Fixing bugs on comments controller and adding respond_to

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,11 @@
+# encoding: utf-8
+
 class Comment < ActiveRecord::Base
+
   belongs_to :user
   belongs_to :topic, :polymorphic => true, :counter_cache => true
   validates_presence_of :text
 
   paginates_per 15
+
 end
