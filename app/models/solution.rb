e271e9147f2bb32599c74332b12b71cc49893fd6
ochko <ochkol@reallyenglish.com>
2013-09-22 20:12:32 +0900

    update points on solution when problem points change

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -149,7 +149,7 @@ class Solution < ActiveRecord::Base
   end
 
   def points_taken
-    passed_ration * problem.point * attempt_bonus
+    percent * problem.point * attempt_bonus
   end
 
   def average_time
--- app/observers/problem_observer.rb
+++ app/observers/problem_observer.rb
@@ -3,7 +3,15 @@ class ProblemObserver < ActiveRecord::Observer
     if problem.changes['contest_id'] && problem.contest && problem.user.email_valid?
       Notifier.problem_selection(problem.user, problem.contest, problem)
     end
+    # point depends on tried_count and solved_count
     if problem.changes['tried_count'] || problem.changes['solved_count']
+      problem.solutions.each do |solution|
+        if solution.contest.nil? || solution.contest.continuing?
+          solution.point = points_taken
+          solution.save!
+        end
+      end
+
       problem.contest.rank! if problem.contest
     end
   end
