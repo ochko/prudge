d16dc9c8851445f9940002de3be2b7190e4e87a7
ochko <ochkol@reallyenglish.com>
2013-11-12 22:59:48 +0900

    sometimes solutions get updated(replaced) while check job is in queue

--- app/jobs/check_solution_job.rb
+++ app/jobs/check_solution_job.rb
@@ -2,6 +2,8 @@ class CheckSolutionJob < BaseJob
   @queue = :high
 
   def self.perform(id)
+    return unless Solution.exists?(id) # solution updated while job is in queue
+
     solution = Solution.find(id)
     sandbox = Sandbox.new(solution)
     sandbox.run
