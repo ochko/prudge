530b9da95319d5d119ecdb77a37e3291006cd364
ochko <ochkol@reallyenglish.com>
2015-05-20 21:00:26 +0900

    fix login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,22 +4,20 @@ class UserSessionsController < ApplicationController
 
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
-  
+
   def new
     @user_session = UserSession.new
   end
-  
+
   def create
     @user_session = UserSession.new(params[:user_session])
-    @user_session.save do |result|
-      if result
-        redirect_back_or_default account_users_url
-      else
-        render :action => :new
-      end
+    if @user_session.save
+      redirect_back_or_default account_users_url
+    else
+      render :action => :new
     end
   end
-  
+
   def destroy
     current_user_session.destroy
     flash_notice
