82e2b110fca517907bfd8e7b151b184b01f9a7b9
Alexandr <alexfox@protonmail.ch>
2015-05-20 17:51:58 +0500

    Removed rank table and all of it's dependencies

--- db/migrate/20130623065520_create_participants.rb
+++ db/migrate/20130623065520_create_participants.rb
@@ -3,22 +3,20 @@ class CreateParticipants < ActiveRecord::Migration
     create_table :participants do |t|
       t.integer :contest_id , :null => false
       t.integer :user_id    , :null => false
-      t.integer :rank
       t.float   :points     , :default => 0.0
     end
 
     connection.select_values("SELECT id FROM contests").each do |contest_id|
-      rank, point, time = 0, 0.0, 0.0
+      point, time = 0.0, 0.0
       connection.select_rows("SELECT user_id, sum(point) AS point, sum(solved_in) AS time FROM solutions WHERE contest_id = #{contest_id} GROUP BY user_id ORDER BY point DESC, time ASC").each do |row|
         user_id, user_point, user_time = row[0], row[1].to_f, row[2].to_f
 
         if (point != user_point) || (user_time - time > 0.1)
           point = user_point
-          rank += 1
         end
 
-        values = [contest_id, user_id, rank, user_point].map{|v| connection.quote v}.join(', ')
-        connection.execute("INSERT INTO participants(contest_id, user_id, rank, points) VALUES(#{values})")
+        values = [contest_id, user_id, user_point].map{|v| connection.quote v}.join(', ')
+        connection.execute("INSERT INTO participants(contest_id, user_id, points) VALUES(#{values})")
       end
     end
 
